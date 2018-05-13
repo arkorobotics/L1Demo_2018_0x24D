374caf3765582dfa251a91fcde26c70b5a7e6900
David Khudaverdyan <khudaverdyan.david@gmail.com>
2018-05-12 18:25:39 -0700

    Updated audio code. Resolved random background noise. Switched audio to SuperSonic Racing for testing. Updated sine table to 512 items.

--- L1Demo_2018_0x24D.X/audio.c
+++ L1Demo_2018_0x24D.X/audio.c
@@ -6,10 +6,12 @@ volatile uint32_t time_sec;		           // Demo Master Clock
 volatile uint32_t time_subsec;
 
 volatile unsigned short ch1_val = 0;       // Audio Channel 1
+volatile unsigned short ch2_val = 0;       // Audio Channel 1
+volatile unsigned short ch3_val = 0;       // Audio Channel 1
 
 void audio_init(void) 
 {
-    PR1 = 0xFF;	// TODO: Add math for calculating PR1 based on sample freq (62.745kHz)
+    PR1 = 0x100;	// TODO: Add math for calculating PR1 based on sample freq (62.745kHz)
     _T1IP = 5;	// set interrupt priority
     _TON  = 1;	// turn on the timer
     _T1IF = 0;	// reset interrupt flag
@@ -27,18 +29,33 @@ void __attribute__((__interrupt__, auto_psv)) _T1Interrupt(void)
 
 	static unsigned short duration = 0;
     static unsigned int ch1_ncount = 0;
+    static unsigned int ch2_ncount = 0;
+    static unsigned int ch3_ncount = 0;
 
     // Do Channel 1 Stuff
-    if (ch1_ncount < 0x7F80)
-    {
-        ch1_ncount += song_ch1f[idx];
-    }
-    else
+    ch1_ncount += song_ch2f[idx] - 1;
+    if (ch1_ncount > 0x803f)
     {
         ch1_ncount = 0;
     }
+    
+    // Do Channel 2 Stuff
+    ch2_ncount += song_ch3f[idx] - 1;
+    if (ch2_ncount > 0x803f)
+    {
+        ch2_ncount = 0;
+    }
+    
+    // Do Channel 3 Stuff
+    ch3_ncount += song_ch4f[idx] - 1;
+    if (ch3_ncount > 0x803f)
+    {
+        ch3_ncount = 0;
+    }
 
     ch1_val = sinetable[ch1_ncount>>6];
+    ch2_val = sinetable[ch2_ncount>>6];
+    ch3_val = sinetable[ch3_ncount>>6];
     
 	// Duration
 	if(duration < 0x7A1)
@@ -50,7 +67,7 @@ void __attribute__((__interrupt__, auto_psv)) _T1Interrupt(void)
 	 	idx++;
 
         // Loop it!
-		if(idx == sizeof(song_ch1f) / sizeof(song_ch1f[0]) ) 
+		if(idx == sizeof(song_ch4f) / sizeof(song_ch4f[0]) ) 
 		{
 			idx = 0;
 		}
@@ -59,12 +76,12 @@ void __attribute__((__interrupt__, auto_psv)) _T1Interrupt(void)
 	}
 	
 	// Mix and set audio
-    PORTB = (ch1_val<<8);
+    PORTB = (ch1_val<<6) + (ch2_val<<6) + (ch3_val<<6);
 
     // Time Keeping
     time_subsec++;
 
-    if(time_subsec >= 62745)
+    if(time_subsec >= 0xf424)
     {
         time_sec++;
         time_subsec = 0;
